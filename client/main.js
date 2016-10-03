c8cc5262a4650525b5c69086d6224e7579f16fe5
Alex Pareto <alex.pareto@gmail.com>
2016-10-02 20:19:37 -0700

    initial init

---
+++ client/main.js
@@ -0,0 +1,46 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+//import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+});
+
+Template.hello.helpers({
+});
+
+Template.hello.events({
+    'click button': function(e) {
+      e.preventDefault();
+
+      var amount = $('input#amount').val();
+      amount = amount.replace(/\$/g, '').replace(/\,/g, '')
+
+      amount = parseFloat(amount);
+
+      if (isNaN(amount)) {
+        $('#error_explanation').html('<p>Please enter a valid amount in USD ($).</p>');
+        return;
+      }
+      else if (amount < 5.00) {
+        $('#error_explanation').html('<p>Sponsorship amount must be at least $5.</p>');
+        return;
+      }
+      else {
+        amount = amount * 100; // Needs to be an integer!
+        StripeCheckout.open({
+        key: Meteor.settings.public.stripe_public,
+        amount: Math.round(amount), // this is equivalent to $50
+        name: 'Scope',
+        description: 'Sponsorship payment',
+        panelLabel: 'Pay',
+        token: function(res) {
+          stripeToken = res.id;
+          console.info(res);
+          Meteor.call('chargeCard', stripeToken, Math.round(amount));
+        }
+      });
+      }
+
+  },
+});
---
+++ server/main.js
@@ -0,0 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+
+
+Meteor.methods({
+    'chargeCard': function(stripeToken, amount) {
+      check(stripeToken, String);
+      var Stripe = StripeAPI(Meteor.settings.stripe_secret);
+
+      Stripe.charges.create({
+        source: stripeToken,
+        amount: amount, // this is equivalent to $50
+        currency: 'usd'
+      }, function(err, charge) {
+        console.log(err, charge);
+      });
+    }
+  });
